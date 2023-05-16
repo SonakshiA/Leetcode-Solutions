df0772ed782826fe2fe512e7806f6bab7d2c548e
Sonakshi Arora <compsci.materials@gmail.com>
2023-05-16 21:37:57 +0530

    Time: 1 ms (85.94%), Space: 42.8 MB (6.96%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countSeniors(String[] details) {
+        int res = 0;
+
+
+        for(int i=0 ;i < details.length; i++){
+            
+            int age = Character.getNumericValue(details[i].charAt(11))*10 + Character.getNumericValue(details[i].charAt(12));
+
+            if(age>60){
+                res++;
+            }
+        }
+        return res;
+    }
+}
