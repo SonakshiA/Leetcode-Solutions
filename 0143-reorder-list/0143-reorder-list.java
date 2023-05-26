74775a2ac5e4c329c6e1e0de4dfce8c535a06af9
Sonakshi Arora <compsci.materials@gmail.com>
2023-05-27 00:57:22 +0530

    Time: 1 ms (100.00%), Space: 46.4 MB (5.40%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.java
@@ -0,0 +1,73 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    
+    private ListNode reverse(ListNode head){
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode forw = null;
+        
+        while(curr!=null){
+            forw = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = forw;
+        }
+        return prev;
+    }
+    
+    
+    private ListNode middle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        while(fast.next!=null && fast.next.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+    
+    public void reorderList(ListNode head) {
+        
+        if(head==null || head.next==null){
+            return;
+        }
+        
+        ListNode mid = middle(head);
+        ListNode k = reverse(mid.next);
+        
+        ListNode c1 = head;
+        ListNode c2 = k;
+        ListNode f1 = null;
+        ListNode f2 = null;
+        mid.next = null; //to split the list into two lists
+        
+        while(c1!=null && c2!=null){
+            f1 = c1.next;
+            f2 = c2.next;
+            c1.next = c2;
+            c2.next = f1;
+            c1 = f1;
+            c2 = f2;
+        }
+    }
+}
+
+/*
+
+Divide the list two halves
+Reverse the second half list
+
+Let A and B be the two lists.
+Connect A0->B0->A1->B1-> ....so on
+*/
