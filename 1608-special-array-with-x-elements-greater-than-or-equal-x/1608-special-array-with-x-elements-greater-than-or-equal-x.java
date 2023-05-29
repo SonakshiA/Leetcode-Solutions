85385327bda0eeb13796d92d0119323805c2de19
Sonakshi Arora <compsci.materials@gmail.com>
2023-05-30 00:18:30 +0530

    Time: 1 ms (68.00%), Space: 39.9 MB (96.34%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        for(int i=0;i<=100;i++){
+            int count=0;
+            for(int num:nums){
+                if(num>=i) count++;
+            }
+            if(i==count) return i;
+        }
+        return -1;
+        
+    }
+}
