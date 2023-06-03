0dac8db04bc22a8ee299ea5b79608d5ac5f3fc18
Sonakshi Arora <compsci.materials@gmail.com>
2023-06-04 00:27:35 +0530

    Time: 204 ms (55.07%), Space: 40.2 MB (97.87%) - LeetHub

---
+++ 0079-word-search/0079-word-search.java
@@ -0,0 +1,44 @@
+class Solution {
+     private boolean searchNext(char[][] board, String word, int row, int col, int index, int m, int n) {
+        if (index == word.length()) {
+            return true;
+        }
+
+        if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word.charAt(index) || board[row][col] == '!') {
+            return false;
+        }
+
+        //prevent reuse of same character
+        char c = board[row][col];
+        board[row][col] = '!';
+
+        boolean top = searchNext(board, word, row - 1, col, index + 1, m, n);
+        boolean bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
+        boolean left = searchNext(board, word, row, col - 1, index + 1, m, n);
+        boolean right = searchNext(board, word, row, col + 1, index + 1, m, n);
+         
+         board[row][col] = c;
+
+        return top || bottom || left || right;
+
+    }
+    
+    public boolean exist(char[][] board, String word) {
+         int m = board.length;
+        
+        int n = board[0].length;
+
+        int index = 0;
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (board[i][j] == word.charAt(index)) {
+                    if (searchNext(board, word, i, j, index, m, n)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
