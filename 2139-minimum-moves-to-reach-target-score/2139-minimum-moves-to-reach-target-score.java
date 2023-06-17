3dbd000760bb39812f5914226f98b27f1d37ef6d
Sonakshi Arora <compsci.materials@gmail.com>
2023-06-18 04:44:22 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (5.53%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.java
@@ -11,7 +11,7 @@ class Solution {
                 target-=1;
             }
         }
-        return x + (target-1);
+        return x + (target-1); //(suppose we have 4 as target and maxDoubles is 0, so we need 4-1 i.e 3 steps to get to 1)
     }
 }
 
