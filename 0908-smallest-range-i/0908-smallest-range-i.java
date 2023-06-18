f4abcae0c37179f3a680f7f205499847b474ebcb
Sonakshi Arora <compsci.materials@gmail.com>
2023-06-19 01:07:34 +0530

    Time: 5 ms (30.79%), Space: 43.9 MB (52.74%) - LeetHub

---
+++ 0908-smallest-range-i/0908-smallest-range-i.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int smallestRangeI(int[] nums, int k) {
+        Arrays.sort(nums);
+        
+        int min = nums[0];
+        int max = nums[nums.length-1];
+        
+        int diff = (max-k) - (min+k);
+        
+        return (diff>0) ? diff: 0;
+    }
+}
