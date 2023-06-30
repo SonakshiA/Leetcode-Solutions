3730deb744a910e134ddfd7402c655d9859e97ef
Sonakshi Arora <compsci.materials@gmail.com>
2023-06-30 20:47:49 +0530

    Time: 24 ms (41.86%), Space: 44.8 MB (12.02%) - LeetHub

---
+++ 2606-find-the-substring-with-maximum-cost/2606-find-the-substring-with-maximum-cost.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int maximumCostSubstring(String s, String chars, int[] vals) {
+        HashMap<Character,Integer> map = new HashMap<>();
+        
+        //make a map
+        for(int i =0; i<chars.length();i++){
+            map.put(chars.charAt(i),vals[i]);
+        }
+        
+        int costMax = Integer.MIN_VALUE;
+        int start = 0;
+        int currSum = 0;
+        for(int i =0 ;i<s.length(); i++){
+            if(map.containsKey(s.charAt(i)))
+                currSum+= map.get(s.charAt(i));
+            else
+                currSum+= s.charAt(i)-'a'+1;
+            
+            costMax = Math.max(currSum,costMax);
+            
+            if(currSum<0)
+                currSum = 0;
+        }
+        return (costMax<0) ? 0 : costMax;
+    }
+}
