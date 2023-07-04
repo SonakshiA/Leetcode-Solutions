38dd7632851ab3bc0a19269d83c43d2dc1c9eb62
Sonakshi Arora <compsci.materials@gmail.com>
2023-07-04 17:24:40 +0530

    Time: 5 ms (98.76%), Space: 44.2 MB (13.28%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.java
@@ -1,29 +1,33 @@
 class Solution {
-    
-    private int dfs(int[][] grid, int startRow, int startCol){
+    private int dfs(int[][] grid, int sr, int sc) {
         int n = grid.length;
         int m = grid[0].length;
-        
-        if(startRow<0 || startRow>=n || startCol<0 || startCol>=m)
-            return 0;
-        
-        if(grid[startRow][startCol]==0)
-            return 0;
-        
-        int fish = grid[startRow][startCol];
-        grid[startRow][startCol] = 0; //visited
-        
-        return fish + dfs(grid,startRow+1,startCol) + dfs(grid,startRow-1,startCol) +               dfs(grid,startRow,startCol-1) + dfs(grid,startRow, startCol+1);
+
+        int fish = grid[sr][sc];
+        grid[sr][sc] = 0;
+
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, 1, 0, -1};
+
+        for (int i = 0; i < 4; i++) {
+            int nRow = sr + delRow[i];
+            int nCol = sc + delCol[i];
+
+            if (nRow >= 0 && nRow < n && nCol >= 0 && nCol < m && grid[nRow][nCol] != 0) {
+                 fish += dfs(grid, nRow, nCol);
+            }
+        }
+        return fish;
     }
+    
     public int findMaxFish(int[][] grid) {
-        int res = 0;
         int n = grid.length;
         int m = grid[0].length;
-        
-        for(int i = 0 ; i<n ; i++){
-            for(int j = 0; j<m; j++){
-                if(grid[i][j]!=0){
-                    res = Math.max(res,dfs(grid,i,j));
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] != 0) {
+                    res = Math.max(res,dfs(grid, i, j));
                 }
             }
         }
