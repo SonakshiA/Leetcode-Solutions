cadd9324c851c4c6907399f1c7fc217d2cf5f879
Sonakshi Arora <compsci.materials@gmail.com>
2023-07-26 14:48:16 +0530

    Time: 16 ms (28.28%), Space: 54.2 MB (99.31%) - LeetHub

---
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] xorQueries(int[] arr, int[][] queries) {
+        int[] ans = new int[queries.length];
+        int[] xors = new int[arr.length+1];
+        
+        for(int i=0;i<arr.length;i++){
+            xors[i+1] ^= xors[i]^arr[i];
+        } 
+        System.out.println(Arrays.toString(xors));
+        int i=0;
+        for(int[] query:queries){
+            ans[i++] = xors[query[0]]^xors[query[1]+1];
+        }
+        return ans;
+    }
+}
