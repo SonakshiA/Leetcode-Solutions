9b8ed98d3eb3d80eb904502ce96d66ce3e6bb5a2
Sonakshi Arora <compsci.materials@gmail.com>
2023-07-26 15:09:29 +0530

    Time: 1 ms (100.00%), Space: 39.8 MB (55.63%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int reverse(int x) {
+        long rev = 0;
+        int copy = x;
+        
+        while(x!=0){
+            int rem = x%10;
+            rev = rev*10 + rem;
+            x/=10;
+        }
+        
+        return (rev>Integer.MAX_VALUE || rev<Integer.MIN_VALUE) ? 0 : (int) rev;
+    }
+}
