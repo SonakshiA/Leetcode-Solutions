cbcc475de67ea1318e6b6d9f5925703eca9c940a
Sonakshi Arora <compsci.materials@gmail.com>
2023-08-18 23:59:20 +0530

    Time: 1 ms (84.90%), Space: 41.4 MB (12.38%) - LeetHub

---
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -0,0 +1,28 @@
+class Solution {
+    private int f(int index,int buy,int[] prices, int[][] dp){
+        if(index>=prices.length){
+            return 0;
+        }
+        
+        if(dp[index][buy]!=-1) return dp[index][buy];
+        
+        int profit = 0;
+        
+        if(buy==1){
+            profit = Math.max(-prices[index] + f(index+1,0,prices,dp),0+f(index+1,1,prices,dp));
+        }else{
+            profit = Math.max(prices[index] + f(index+2,1,prices,dp),0+f(index+1,0,prices,dp));
+        }
+        
+        return dp[index][buy] = profit;
+    }
+    public int maxProfit(int[] prices) {
+        int[][] dp = new int[prices.length][2];
+        
+        for(int[] row:dp){
+            Arrays.fill(row,-1);
+        }
+        
+        return f(0,1,prices,dp);
+    }
+}
