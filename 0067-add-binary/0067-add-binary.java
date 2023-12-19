0aa61d5ca6ba5c56d8fe82e06344809e9cf9a372
Sonakshi Arora <compsci.materials@gmail.com>
2023-12-19 23:50:51 +0530

    Time: 1 ms (100.00%), Space: 40.9 MB (86.41%) - LeetHub

---
+++ 0067-add-binary/0067-add-binary.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int carry = 0; //initially no carry
+        int i = a.length()-1;
+        int j=b.length()-1;
+        
+        StringBuilder sb = new StringBuilder();
+        
+        while(i>=0 || j>=0 || carry==1){
+            if(i>=0){
+                carry+= (a.charAt(i)-'0');
+                i--;
+            }
+            if(j>=0){
+                carry+= (b.charAt(j)-'0');
+                j--;
+            }
+            sb.append(carry%2);
+            carry/=2;
+        }
+        return sb.reverse().toString();
+    }
+}
+
