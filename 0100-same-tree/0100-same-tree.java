484a12b3b18db7c436d1791e6d2de3b01d3bdc06
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-07 21:42:25 +0530

    Time: 0 ms (100.00%), Space: 41.4 MB (9.63%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,17 +14,16 @@
  * }
  */
 class Solution {
+    
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null)
             return true;
-        
-        if(p==null && q!=null || p!=null && q==null)
+
+        if((p==null && q!=null) || (p!=null && q==null))
             return false;
         
-        if(p.val==q.val){
-            if(isSameTree(p.left,q.left) && isSameTree(p.right,q.right))
-                return true;
-        }
-        return false; 
+        if(p.val==q.val)
+            return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        return false;
     }
 }
