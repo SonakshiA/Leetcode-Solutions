67a694d5bf4e39c33d857daae572702c20dac34a
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-08 01:26:05 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (82.26%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -15,14 +15,13 @@
  */
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return isValid(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return checkBST(root,Long.MIN_VALUE,Long.MAX_VALUE);
     }
     
-    private boolean isValid(TreeNode root, long min, long max){
-        if (root==null) return true;
+    public boolean checkBST(TreeNode root, long min, long max){
+        if(root==null) return true;
+        if(root.val<=min || root.val>=max) return false;
         
-        if(root.val>=max || root.val<=min) return false;
-        
-        return isValid(root.left,min,root.val) && isValid(root.right,root.val,max); //important
+        return checkBST(root.left,min,root.val) && checkBST(root.right,root.val,max);
     }
 }
