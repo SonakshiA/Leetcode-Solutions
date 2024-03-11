9fb904472d029bd19387e23ec1dce81d5ce4b253
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-11 21:37:30 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (70.00%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,25 +1,16 @@
 class Solution {
-    private int solve(int n, int[] dp){
+    int f(int n, int[] dp){
         if(n<=2)
             return n;
-        
-        if(dp[n]!=-1)
-            return dp[n];
-        return dp[n] = solve(n-1,dp) + solve(n-2,dp);
+        if(dp[n]!=-1) return dp[n];
+        return dp[n] = f(n-1,dp) + f(n-2,dp);
     }
+    
     public int climbStairs(int n) {
-       int[] dp = new int[n+1];
-       Arrays.fill(dp,-1);
-       return solve(n,dp);
-       
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return f(n,dp);
     }
 }
 
-/*
-
-if n==0, 0 ways to climb
-if n==1, 1 way to climb
-if n==2, 2 ways to climb (take 1 jump or 2 jumps)
-
-*/
-
+//fibonacci series question
