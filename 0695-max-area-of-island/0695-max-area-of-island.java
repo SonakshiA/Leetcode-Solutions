8b6988a3067c08a686e938f6c1eaeccb8da96e09
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 01:34:49 +0530

    Time: 5 ms (9.48%), Space: 45.2 MB (6.48%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,33 +1,57 @@
+class Pair{
+    int row;
+    int col;
+    
+    public Pair(int r, int c){
+        row = r;
+        col = c;
+    }
+}
+
 class Solution {
-    private int dfs(int[][] grid, int[][] vis,int sr,int sc){
+    int ans = 0;
+    public int bfs(int[][] grid, int[][] vis, int startRow, int startCol){
+        int size=1;
+        vis[startRow][startCol] = 1;
         int n = grid.length;
         int m = grid[0].length;
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(startRow,startCol));
         
-        if(sr<0 || sr>=n || sc<0 || sc>=m){
-            return 0;
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.remove();
+            for(int delRow=-1;delRow<=1;delRow++){
+                for(int delCol=-1;delCol<=1;delCol++){
+                    
+                    if((delRow==-1 && delCol==-1) ||(delRow==-1 && delCol==1) ||(delRow==1 && delCol==-1) ||(delRow==1 && delCol==1)){
+                        continue;
+                    }
+                    int nRow = delRow + row;
+                    int nCol = delCol + col;
+                    
+                    if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && grid[nRow][nCol]==1 && vis[nRow][nCol]==0){
+                        vis[nRow][nCol]=1;
+                        q.add(new Pair(nRow,nCol));
+                        size++;
+                    }
+                }
+            }
         }
-        
-        if(grid[sr][sc]==0){
-            return 0;
-        }
-        if(vis[sr][sc]==1){
-            return 0;
-        }
-        
-        vis[sr][sc]=1;
-        
-        return 1+dfs(grid,vis,sr+1,sc) + dfs(grid,vis,sr-1,sc) + dfs(grid,vis,sr,sc+1) +                                dfs(grid,vis,sr,sc-1);
+        return size;
     }
+    
     public int maxAreaOfIsland(int[][] grid) {
         int n = grid.length;
         int m = grid[0].length;
-        int ans = 0;
-        int[][] vis = new int[n][m];
+        int vis[][] = new int[n][m];
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(grid[i][j]==1 && vis[i][j]==0){
-                    ans  = Math.max(ans,dfs(grid,vis,i,j));
+                    int size = bfs(grid,vis,i,j);
+                    ans = Math.max(ans,size);
                 }
             }
         }
