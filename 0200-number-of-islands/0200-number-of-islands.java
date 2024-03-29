8977f67ed3eb216c6e0ce6e0de995f86c034ab58
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 00:56:44 +0530

    Time: 8 ms (15.09%), Space: 51.8 MB (18.25%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -2,60 +2,49 @@ class Pair{
     int first;
     int second;
     
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
+    public Pair(int f, int s){
+        first = f;
+        second = s;
     }
 }
-
 class Solution {
-    
-    private void bfs(int[][] vis, char[][] grid, int startRow, int startCol){
-        vis[startRow][startCol] =1;
-        
-        Queue<Pair> q = new LinkedList<Pair>();
-        q.add(new Pair(startRow, startCol));
+    public void bfs(char[][] grid, int[][] vis, int startRow, int startCol){
+        vis[startRow][startCol] = 1;
         
         int n = grid.length;
         int m = grid[0].length;
+        
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(startRow, startCol));
         
         while(!q.isEmpty()){
             int row = q.peek().first;
             int col = q.peek().second;
             q.remove();
             
-            for(int delRow = -1; delRow<=1; delRow++){
-                for(int delCol = -1; delCol<=1; delCol++){
-                                      
-                    if((delRow==-1 && delCol==-1) || (delRow== -1 && delCol==1) || (delRow==1 && delCol                     == -1) || (delRow==1 && delCol==1)){
+            for(int delRow=-1;delRow<=1;delRow++){
+                for(int delCol=-1;delCol<=1;delCol++){
+
+                    if((delRow==-1 && delCol==-1) || (delRow==-1 && delCol==1) || (delRow==1 && delCol==-1) || (delRow==1 && delCol==1))
                         continue;
-                    }
-                    
-                    
-                    int nRow = row + delRow;
-                    int nCol = col + delCol;
-                    
+                    int nRow = delRow + row;
+                    int nCol = delCol + col;
+
                     if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && grid[nRow][nCol]=='1' && vis[nRow][nCol]==0){
                         vis[nRow][nCol] = 1;
                         q.add(new Pair(nRow,nCol));
                     }
                 }
-            } 
-            
-        } 
-        
+            }
+        }  
     }
-    
     public int numIslands(char[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-        int count = 0;
-        int[][] vis = new int[n][m];
-        
-        for(int i = 0; i<n ; i++){
-            for(int j = 0; j<m ; j++){
-                if(vis[i][j] == 0 && grid[i][j]=='1'){
-                    bfs(vis,grid,i,j);
+        int count=0;
+        int[][] vis = new int[grid.length][grid[0].length];
+        for(int i=0;i<grid.length;i++){
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    bfs(grid,vis,i,j);
                     count++;
                 }
             }
