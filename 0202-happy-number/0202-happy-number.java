aa6f1e1ab8ab1c701b46e5f21a86c00e219acbf0
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 19:33:35 +0530

    Time: 1 ms (64.13%), Space: 40.7 MB (37.45%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -8,14 +8,14 @@ class Solution {
         return sum_of_sq;
     }
     public boolean isHappy(int n) {
-        int slow = n;
-        int fast = n;
-        
-        do{
-            slow = square(slow);
-            fast = square(square(fast));
-        }while(slow!=fast);
-        
-        return slow==1;
+       HashSet<Integer> set = new HashSet<>();
+        while(true){
+            n = square(n);
+            if(n==1)
+                return true;
+            else if(set.contains(n))
+                return false;
+            set.add(n);
+        }
     }
 }
