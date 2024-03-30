c47ce97768a0b2df1962ccdd8e2e297981c08f98
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 20:06:52 +0530

    Time: 1 ms (73.58%), Space: 45.3 MB (75.30%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,46 +1,36 @@
 class Pair{
-    int first;
-    int second;
-    public Pair(int first, int second){
-        this.first = first;
-        this.second = second;
+    int row;
+    int col;
+    
+    public Pair(int r, int c){
+        row = r;
+        col = c;
     }
 }
 
-
 class Solution {
-    private void convert(int[][] matrix, int sr, int sc){
-        int n = matrix.length;
-        int m = matrix[0].length;
-        
-        for(int i = 0; i<m ;i++){
-            matrix[sr][i] = 0;
-        }
-        
-        for(int j = 0; j<n; j++){
-            matrix[j][sc] = 0;
-        }
-      
-    }
-    
-    
     public void setZeroes(int[][] matrix) {
+        Queue<Pair> q = new LinkedList<>();
         int n = matrix.length;
         int m = matrix[0].length;
-        
-        ArrayList<Pair> l = new ArrayList<>();
         
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(matrix[i][j]==0){
-                    l.add(new Pair(i,j));
-                }
+                if(matrix[i][j]==0)
+                    q.add(new Pair(i,j));
             }
         }
         
-        for(Pair p:l){
-            System.out.println(p.first + " " + p.second);
-            convert(matrix,p.first, p.second);
+        while(!q.isEmpty()){
+            int row = q.peek().row;
+            int col = q.peek().col;
+            q.remove();
+            for(int j=0;j<m;j++){
+                matrix[row][j]=0;
+            }
+            for(int i=0;i<n;i++){
+                matrix[i][col] = 0;
+            }
         }
     }
 }
