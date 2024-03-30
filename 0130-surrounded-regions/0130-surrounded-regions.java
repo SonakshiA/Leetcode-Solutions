2d044ad6bbd3604b6f5de21c9edfecd2e6211abd
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 18:25:21 +0530

    Time: 2 ms (88.62%), Space: 45.2 MB (62.62%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -1,68 +1,45 @@
 class Solution {
-    
-    private void dfs(char[][] board, int[][] vis, int startRow, int startCol){
-        vis[startRow][startCol] = 1;
+    private void dfs(char[][] board, int[][] vis, int row, int col, int n, int m){
+        vis[row][col] = 1;
+        int[] delRow = {-1,0,1,0};
+        int[] delCol = {0,1,0,-1};
         
-        int n = board.length;
-        int m = board[0].length;
-        
-        for(int delRow=-1; delRow<=1; delRow++){
-            for(int delCol=-1; delCol<=1; delCol++){
-                
-                //ignore diagnol elements
-                 if((delRow==-1 && delCol==-1) || (delRow== -1 && delCol==1) || (delRow==1 && delCol                     == -1) || (delRow==1 && delCol==1)){
-                        continue;
-                    }
-                int nRow = startRow + delRow;
-                int nCol = startCol + delCol;
-                
-                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && vis[nRow][nCol]==0 && board[nRow][nCol] ==                 'O'){
-                    dfs(board,vis,nRow,nCol);
-                }
+        for(int i=0;i<4;i++){
+            int nRow = row + delRow[i];
+            int nCol = col + delCol[i];
+            
+            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && board[nRow][nCol]=='O' && vis[nRow][nCol]==0){
+                dfs(board,vis,nRow,nCol,n,m);
             }
         }
-        
     }
-    
     public void solve(char[][] board) {
-       int n = board.length;
+        int n = board.length;
         int m = board[0].length;
         int[][] vis = new int[n][m];
         
-        //checking the first and last row (boundary)
-        for(int j = 0 ; j<m; j++){
+        for(int j=0;j<m;j++){
             if(board[0][j] == 'O' && vis[0][j]==0){
-                dfs(board,vis,0,j);
+                dfs(board,vis,0,j,n,m);
             }
-            
-            if(vis[n-1][j]==0 && board[n-1][j]=='O'){
-                dfs(board,vis,n-1,j);
+            if(board[n-1][j] == 'O' && vis[n-1][j]==0){
+                dfs(board,vis,n-1,j,n,m);
             }
         }
-        
-        //checking the first and last column (boundary)
-        for(int i = 0; i<n ; i++){
+        for(int i=0;i<n;i++){
             if(board[i][0] == 'O' && vis[i][0]==0){
-                dfs(board,vis,i,0);
+                dfs(board,vis,i,0,n,m);
             }
-            
             if(board[i][m-1]=='O' && vis[i][m-1]==0){
-                dfs(board,vis,i,m-1);
+                dfs(board,vis,i,m-1,n,m);
             }
         }
-        
-        for(int i = 0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                if(board[i][j] == 'O' && vis[i][j]==0){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O' && vis[i][j]==0){
                     board[i][j] = 'X';
                 }
             }
         }
     }
 }
-
-/*
-
-mark those Os to Xs which aren't connected to the boundary 'O' 
-
-*/
