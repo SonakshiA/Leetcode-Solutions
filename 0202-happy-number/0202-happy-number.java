17c4019612805e6673f8188e33811f248574ca95
Sonakshi Arora <compsci.materials@gmail.com>
2024-03-30 19:26:45 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (94.45%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,24 +1,21 @@
 class Solution {
-    
-    private int squareOfDigits(int n){
-        int ans = 0;
-        while(n>0){
-            ans += (n%10)*(n%10);
-            n/=10;
+    private int square(int num){
+        int sum_of_sq = 0;
+        while(num!=0){
+            sum_of_sq += (num%10)*(num%10);
+            num/=10;
         }
-        return ans;
+        return sum_of_sq;
     }
-    
     public boolean isHappy(int n) {
         int slow = n;
         int fast = n;
         
         do{
-            slow = squareOfDigits(slow);
-            fast = squareOfDigits(squareOfDigits(fast));
+            slow = square(slow);
+            fast = square(square(fast));
         }while(slow!=fast);
         
         return slow==1;
-        
     }
 }
