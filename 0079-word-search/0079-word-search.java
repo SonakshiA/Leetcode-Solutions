682d886a2373cba3281c0f8285f560f195d9b939
Sonakshi Arora <compsci.materials@gmail.com>
2024-04-01 16:36:43 +0530

    Time: 146 ms (57.83%), Space: 41.7 MB (39.99%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,41 +1,32 @@
 class Solution {
-     private boolean searchNext(char[][] board, String word, int row, int col, int index, int m, int n) {
-        if (index == word.length()) {
+    private boolean backtrack(char[][] board, int row, int col, int index, int n, int m, String word){
+        //this basecase should always appear first
+        if(index==word.length()){
             return true;
         }
-
-        if (row < 0 || col < 0 || row == m || col == n || board[row][col] != word.charAt(index) || board[row][col] == '!') {
+        if(row<0 || row==n || col<0 || col==m || board[row][col]=='!' || board[row][col]!=word.charAt(index)){
             return false;
         }
-
-        //prevent reuse of same character
         char c = board[row][col];
         board[row][col] = '!';
-
-        boolean top = searchNext(board, word, row - 1, col, index + 1, m, n);
-        boolean bottom = searchNext(board, word, row + 1, col, index + 1, m, n);
-        boolean left = searchNext(board, word, row, col - 1, index + 1, m, n);
-        boolean right = searchNext(board, word, row, col + 1, index + 1, m, n);
-
-        board[row][col] = c;
-    
-        return top || bottom || left || right;
-
-    }
-    
-    public boolean exist(char[][] board, String word) {
-         int m = board.length;
         
-        int n = board[0].length;
-
+        boolean top = backtrack(board,row-1,col,index+1,n,m,word);
+        boolean bottom = backtrack(board,row+1,col,index+1,n,m,word);
+        boolean left = backtrack(board,row,col-1,index+1,n,m,word);
+        boolean right = backtrack(board, row,col+1,index+1,n,m,word);
+        
+        board[row][col] = c;
+        return top || bottom || left || right;
+    }
+    public boolean exist(char[][] board, String word) {
+        int n = board.length;
+        int m = board[0].length;
         int index = 0;
-
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == word.charAt(index)) {
-                    if (searchNext(board, word, i, j, index, m, n)) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] == word.charAt(index)){
+                    if(backtrack(board,i,j,index,n,m,word))
                         return true;
-                    }
                 }
             }
         }
