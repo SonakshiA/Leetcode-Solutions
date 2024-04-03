0614c2e2fd33eeae2f1470a6252e1ff9062782bb
Sonakshi Arora <compsci.materials@gmail.com>
2024-04-04 01:18:49 +0530

    Time: 5 ms (69.99%), Space: 47.6 MB (46.54%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.java
+++ 0518-coin-change-ii/0518-coin-change-ii.java
@@ -1,28 +1,24 @@
 class Solution {
-    
-    private int f(int index, int target, int[] coins,int[][] dp){
+    int f(int[][] dp, int index, int target, int[] coins){
         if(index==0){
-            return target%coins[0]==0?1:0;
+            return (target%coins[index]==0?1:0);
         }
         
-        if(dp[index][target]!=-1) return dp[index][target];
+        if (dp[index][target]!=-1) return dp[index][target];
         
-        int notPick = f(index-1,target,coins,dp);
+        int notPick= f(dp,index-1,target,coins);
         int pick = 0;
-        
         if(coins[index]<=target){
-            pick = f(index,target-coins[index],coins,dp);
+            pick = f(dp,index,target-coins[index],coins);
         }
         
-        return dp[index][target] = pick+notPick;
+        return dp[index][target] = pick + notPick;
     }
     public int change(int amount, int[] coins) {
         int[][] dp = new int[coins.length][amount+1];
-        
         for(int[] row:dp){
             Arrays.fill(row,-1);
         }
-        
-        return f(coins.length-1,amount,coins,dp);
+        return f(dp,coins.length-1,amount,coins);
     }
 }
