5a07ef05fd6bcafc6c672c88500e7ccc42470746
Sonakshi Arora <compsci.materials@gmail.com>
2024-04-04 18:13:55 +0530

    Time: 1 ms (80.56%), Space: 42 MB (23.92%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.java
@@ -1,28 +1,25 @@
 class Solution {
-    private int f(int index,int buy,int[] prices, int[][] dp){
-        if(index>=prices.length){
+    public int f(int[] prices, int index, int buy, int[][] dp){
+        if (index>=prices.length)
             return 0;
-        }
         
         if(dp[index][buy]!=-1) return dp[index][buy];
         
         int profit = 0;
-        
-        if(buy==1){
-            profit = Math.max(-prices[index] + f(index+1,0,prices,dp),0+f(index+1,1,prices,dp));
-        }else{
-            profit = Math.max(prices[index] + f(index+2,1,prices,dp),0+f(index+1,0,prices,dp));
+        if(buy==1){ //we can buy a stock
+            profit = Math.max(-prices[index] + f(prices,index+1,0,dp), 0 + f(prices,index+1,1,dp));
+        }else{ //we cannot buy, we must sell the stock
+            //if we sell, we go to index+2 and not index+1 since we cannot buy the day after selling
+            profit = Math.max(prices[index] + f(prices,index+2,1,dp), 0 + f(prices,index+1,0,dp));
         }
-        
         return dp[index][buy] = profit;
     }
     public int maxProfit(int[] prices) {
-        int[][] dp = new int[prices.length][2];
+        int[][] dp = new int[prices.length][2]; //buy can be 0 and 1, so [2]
         
-        for(int[] row:dp){
+        for(int[] row:dp)
             Arrays.fill(row,-1);
-        }
         
-        return f(0,1,prices,dp);
+        return f(prices,0,1,dp);
     }
 }
