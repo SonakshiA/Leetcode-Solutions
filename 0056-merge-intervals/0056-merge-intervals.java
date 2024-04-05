da08a38fdd4ee6211b3344836104e87638f67e5a
Sonakshi Arora <compsci.materials@gmail.com>
2024-04-05 15:31:12 +0530

    Time: 10 ms (30.59%), Space: 46.7 MB (15.34%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,31 +1,33 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        if (intervals.length<=1)
-            return intervals;
-        
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        
-        List<int[]> output_array = new ArrayList<>();
-        int[] current_interval = intervals[0];
-        output_array.add(current_interval);
-        
-        for(int[] interval: intervals){
-            int current_begin = current_interval[0];
-            int current_end = current_interval[1];
-            int next_begin = interval[0];
-            int next_end = interval[1];
+    public List<List<Integer>> mergeIntervals(int[][] arr){
+        int n = arr.length;
+        //sort based on start time
+        Arrays.sort(arr, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                return a[0]-b[0];
+            }
+        });
             
-            if(current_end>=next_begin){
-                //modifies in the output array itself
-                current_interval[1] = Math.max(current_end,next_end);
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        for(int i=0;i<n;i++){
+            if(ans.size()==0 || arr[i][0]>ans.get(ans.size()-1).get(1)){
+                ans.add(Arrays.asList(arr[i][0],arr[i][1]));
             }else{
-                //change the current_interval from intervals[0] to new interval
-                current_interval = interval;
-                output_array.add(current_interval);
+                ans.get(ans.size()-1).set(1,Math.max(ans.get(ans.size()-1).get(1),arr[i][1]));
             }
         }
+        return ans;
+    }
+    
+    public int[][] merge(int[][] intervals) {
+       List<List<Integer>> ans = mergeIntervals(intervals);
+       int[][] res = new int[ans.size()][2];
         
-        return output_array.toArray(new int[output_array.size()][]);
+        for(int i=0;i<ans.size();i++){
+            res[i][0] = ans.get(i).get(0);
+            res[i][1] = ans.get(i).get(1);
+        }
+        return res;
     }
 }
-/* Refer: https://www.youtube.com/watch?v=qKczfGUrFY4 */
