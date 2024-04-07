b76641b95130cff9be00cd14ef208a23a3c22018
Sonakshi Arora <compsci.materials@gmail.com>
2024-04-07 16:07:35 +0530

    Time: 1 ms (99.44%), Space: 56.7 MB (77.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,16 +1,15 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-       int sum = 0;
-       int max = Integer.MIN_VALUE;
-
-        for (int x : nums) {
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int x: nums){
             sum += x;
-            max = Math.max(sum, max);
-            if (sum < 0) sum = 0;
+            max = Math.max(sum,max);
+            if(sum<0)
+                sum = 0;
         }
         return max;
     }
 }
 
-
-//KADANES ALGORITHM USED
+//Kadanes algorithm
