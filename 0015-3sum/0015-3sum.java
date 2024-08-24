e3075f4bef8b79bb56562216419440ebd1ef39bc
Sonakshi Arora <compsci.materials@gmail.com>
2024-08-25 02:53:32 +0530

    Time: 31 ms (57.06%), Space: 52.6 MB (20.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,33 +2,28 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-            if (i == 0 || i > 0 && nums[i - 1] != nums[i]) {
-                int low = i + 1;
-                int high = nums.length - 1;
-                int sum = 0 - nums[i];
-
-                while (low < high) {
-                    if (nums[low] + nums[high] == sum) {
-                        ArrayList<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[low]);
-                        temp.add(nums[high]);
-
-                        res.add(temp);
-
-                        while (low < high && nums[low + 1] == nums[low]) low++;
-                        while (low < high && nums[high - 1] == nums[high]) high--;
-
-                        low++;
-                        high--;
-                    } else if (nums[low] + nums[high] < sum) low++;
-
-                    else high--;
-                }
+        
+        for(int i=0;i+2<nums.length;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue; //remove duplicates
+            int l = i+1;
+            int r = nums.length-1;
+            
+            while(l<r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum==0){
+                    List<Integer> ans = new ArrayList<>();
+                    ans.add(nums[i]);
+                    ans.add(nums[l]);
+                    ans.add(nums[r]);
+                    res.add(ans);
+                    l++;
+                    r--;
+                    while(l<r && nums[l]==nums[l-1]) l++;
+                    while(l<r && nums[r]==nums[r+1]) r--;
+                } else if(sum<0) l++;
+                else r--;
             }
-        }
+        } 
         return res;
     }
 }
