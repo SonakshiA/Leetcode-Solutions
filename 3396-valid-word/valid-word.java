772ccdda3f3c9755bd05577615a5ea1493cc6cdb
Sonakshi Arora <compsci.materials@gmail.com>
2025-07-15 11:05:53 +0530

    Time: 2 ms (30.46%) | Memory: 42.1 MB (62.25%) - LeetSync

--- 3396-valid-word/valid-word.java
+++ 3396-valid-word/valid-word.java
@@ -3,9 +3,6 @@ class Solution {
         boolean hasVowel = false;
         boolean hasConsonant = false;
 
-        Set<Character> vowelSet = new HashSet<>();
-        Set<Character> consonantSet = new HashSet<>();
-
         if(word.length() < 3) {
             return false;
         }
@@ -19,15 +16,12 @@ class Solution {
                 if(c=='a' || c=='A' || c=='e' || c=='E' ||
                 c=='i' || c=='I' || c=='o' || c=='O' ||
                 c=='u' || c=='U') {
-                    vowelSet.add(c);
                     hasVowel = true;
                 } else {
-                    consonantSet.add(c);
                     hasConsonant = true;
                 }
             }
         }
-        //return !vowelSet.isEmpty() && !consonantSet.isEmpty();
         return hasVowel && hasConsonant;
     }
 }
