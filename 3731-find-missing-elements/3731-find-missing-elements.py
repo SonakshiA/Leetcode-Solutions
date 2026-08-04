e23ce36d88e2547a39ba1b4fc5a5bceae803f870
Sonakshi Arora <compsci.materials@gmail.com>
2026-08-04 14:45:07 +0530

    Time: 5 ms (61.52%), Space: 12.2 MB (99.54%) - LeetHub - Time: O(n log n), Space: O(n)

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def findMissingElements(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans = []
+        nums.sort()
+        minimum = nums[0]
+        maximum = nums[len(nums) - 1]
+
+        for i in range(minimum,maximum):
+            if i not in nums:
+                ans.append(i)
+        
+        return ans
+
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
