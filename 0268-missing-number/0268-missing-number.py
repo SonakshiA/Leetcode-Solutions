162550455b1bb300f47e23dee6e20c9b67a90e5f
Sonakshi Arora <compsci.materials@gmail.com>
2026-08-05 12:03:03 +0530

    Time: 0 ms (100%), Space: 13.2 MB (67.88%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0268-missing-number/0268-missing-number.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n = len(nums)
+        sum_actual = sum(nums)
+        sum_ap = n*(n+1)//2
+
+        return sum_ap - sum_actual
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
