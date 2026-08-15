f4120bffec8e02c43ecb089f7c5bd1dc750d16df
Sonakshi Arora <compsci.materials@gmail.com>
2026-08-16 01:55:01 +0530

    Time: 3 ms (38.93%), Space: 43.5 MB (69.71%) - LeetHub - Time: O(n), Space: O(1)

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,22 +1,20 @@
 class Solution {
     public int maximumLengthSubstring(String s) {
-        int start = 0;
-        int n = s.length();
-        int maxWindow = 0;
-        while(start<n){
-            int end = start;
-            int[] count = new int[26];
-            while(end<n){
-                count[s.charAt(end)-'a']++;
-                if(count[s.charAt(end)-'a']>2){
-                    break;
-                }
-                end++;
+        int start = -1, ans = 0;
+        int len = s.length();
+        HashMap<Character, Integer> freq = new HashMap<>();
+        for(int end = 0; end<len; end++) {
+            freq.put(s.charAt(end), freq.getOrDefault(s.charAt(end),0)+1);
+            while(freq.get(s.charAt(end)) > 2) {
+                start++;
+                freq.put(s.charAt(start), freq.getOrDefault(s.charAt(start),0)-1);
             }
-            start++;
-            maxWindow = Math.max(maxWindow, end-start+1);
-
+            ans = Math.max(ans, end - start);
         }
-        return maxWindow;
+    return ans;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
