746fb6e7112b3585844e2f46d9606a46e4e88ede
Sonakshi Arora <compsci.materials@gmail.com>
2026-08-30 10:50:29 +0530

    Time: 2 ms (100%), Space: 86.4 MB (89.09%) - LeetHub - Time: O(n), Space: O(1)

--- 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
+++ 2091-removing-minimum-and-maximum-from-array/2091-removing-minimum-and-maximum-from-array.java
@@ -1,47 +1,33 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
-        int minIndex=0;
-        int maxIndex=0;
-        
-        int minVal = Integer.MAX_VALUE;
-        int maxVal = Integer.MIN_VALUE;
-        
-        for(int i =0; i<nums.length;i++){
-            if(nums[i]<minVal){
-                minVal=nums[i];
-                minIndex=i;
+        // find indices
+        int min = Integer.MAX_VALUE;
+        int index_min = 0;
+        int max = Integer.MIN_VALUE;
+        int index_max = 0;
+        int len = nums.length;
+
+        for(int i = 0 ; i < len ; i++) {
+            if (nums[i] < min) {
+                min = nums[i];
+                index_min = i;
+            } if (nums[i] > max) {
+                max = nums[i];
+                index_max = i;
             }
         }
-        
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]>maxVal){
-                maxVal=nums[i];
-                maxIndex=i;
-            }
-        }
-        
-        int left = Math.min(minIndex,maxIndex);
-        int right = Math.max(minIndex,maxIndex);
-        
-        int op1 = right+1;
-        int op2 = nums.length-left;
-        int op3 = (left+1) + (nums.length-right);
-        
-        System.out.println(op1 + " " + op2 + " " + op3);
-        
-        if(op1<= op2 && op1<=op3)
-            return op1;
-        if(op2<=op3 && op2<=op1)
-            return op2;
-        return op3;
+
+        // once we have indices, there are 3 ways we can find
+        // all from front, all from back, from both front and back
+
+        int front_and_back = len - Math.max(index_min, index_max) + Math.min(index_min, index_max) + 1;
+        int front = Math.max(index_min, index_max) + 1;
+        int back = len - Math.min(index_min, index_max); 
+
+        return Math.min(front_and_back, Math.min(front,back));
     }
 }
 
-/*
-either both from start
-either both from end
-one from start and one from end
-
-[2,10,7,5,4,1,8,6]
-2 left 7 5 4 right 8 6
-*/
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
